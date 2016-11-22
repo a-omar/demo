82dbd0c9cab8780dde3da2a772e191daa176df77
A. Omar <omara@hr.nl>
2016-11-22 22:01:51 +0100

    changed import

--- test_bar.py
+++ test_bar.py
@@ -1,7 +1,11 @@
 from unittest import TestCase
-from demo.demo import Bar
+from demo import Bar
 
 class TestBar(TestCase):
     def test_get_players(self):
         b = Bar()
         self.assertEquals(b.get_players(),10)
+
+
+if __name__ == '__main__':
+    TestBar().test_get_players()
