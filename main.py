193b4d9f8b20c5090571d678ce42ca6369b9bece
A. Omar <omara@hr.nl>
2016-11-22 16:26:59 +0100

    init commit

---
+++ main.py
@@ -0,0 +1,46 @@
+#This module provides the infrastructure for defining abstract base classes
+from abc import ABC, abstractmethod
+
+class Abstract(ABC):
+    @abstractmethod
+    def foo(self):
+        print("foo not implemented yet!")
+        pass
+
+class Derived(Abstract):
+    def foo(self):
+        #TODO: foo not implemented yet
+        super().foo()
+
+
+a  = Derived()
+a.foo()
+
+def upgrade_players():
+    #TODO: update all players in the list l
+    pass
+
+def update_enemies():
+    # TODO: update all enemies in the list l
+    pass
+
+#Option1 creating an empty function
+class Foo:
+    # TODO:
+    pass
+
+#Option2
+class Bar:
+    def get_enemies(self):
+        #TODO:
+        return False
+
+    def get_players(self):
+        #TODO
+        return 10
+
+f = Foo()
+b = Bar()
+
+print(f)
+print(b.get_players())
